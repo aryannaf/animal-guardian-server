b39fe87bd79494cda1259cf97fac4b89be62436e
aryannaf <aryannafarhadi@gmail.com>
2022-06-28 17:10:51 -0400

    Renamed seeds folder and added env sample file

---
+++ migrations/20220628200300_species_and_categories_table.js
@@ -0,0 +1,37 @@
+// exports.up = async(knex) => {
+//     return knex.schema.alterTable('categories', (table) => {
+//         table.string('categoryName').notNullable();
+//     })
+// }
+
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('categories', (table) => {
+        table.string('id').unique().primary();
+        table.string('categoryName').notNullable();
+      })
+      .createTable('species', (table) => {
+        table.string('id').unique().primary();
+        table.string('category_id').notNullable();
+        table.string('name').notNullable();
+        table.string('image').notNullable();
+        table.string('scientificName').notNullable();
+        table.string('category').notNullable();
+        table.string('population').notNullable();
+        table.string('habitat').notNullable();
+        table.text('information').notNullable();
+        table.string('source').notNullable();
+        table.text('petition').notNullable();
+        table.text('donate').notNullable();
+        table
+          .foreign('category_id')
+          .references('id')
+          .inTable('categories')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('species').dropTable('categories');
+  };
